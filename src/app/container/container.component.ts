73364237088e37150acaaf73a52bba452f808d73
kiranbhoi <kiran.bhoi@parallel-minds.com>
2023-05-24 12:26:01 +0530

    new component generated

---
+++ src/app/container/container.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-container',
+    templateUrl: './container.component.html',
+    styleUrls: ['./container.component.css']
+    
+})
+
+// we need to register this component in app.module.ts file
+export class ContainerComponent{
+
+}
